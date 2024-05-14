7b003719937b7af77a47937e65b85330babebdef
BaconAgent <stoiandimitrov03@gmail.com>
2024-05-14 13:26:29 +0200

    Made docker files and implemented log in and log out functions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React from "react";
+import { Auth0Provider } from "@auth0/auth0-react";
 import "./App.css";
 import Reserve from "./Reserve";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButon";
 
 function App() {
   return (
-    <div className="Hello World">
-      <Reserve />
-    </div>
+    <Auth0Provider
+      domain="dev-twctg5mxoooke8y4.eu.auth0.com"
+      clientId="vZyAmgrtmo7G5bplqYjXCIwsimVNCXxc"
+      redirectUri={window.location.origin}
+    >
+      <div className="App">
+        <LoginButton />
+        <LogoutButton />
+        <Reserve />
+      </div>
+    </Auth0Provider>
   );
 }
 
